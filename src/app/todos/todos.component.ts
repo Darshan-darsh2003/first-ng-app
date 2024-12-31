ccb9bbafe5a25b4cab105b586fd63a0b5c880879
Darshan-darsh2003 <131742631+Darshan-darsh2003@users.noreply.github.com>
2024-12-31 15:25:18 +0530

    feat: add Todo component and service with API integration

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { TodosService } from './../services/todos.service';
+import { Todo } from '../components/models/todo.type';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
   imports: [],
   templateUrl: './todos.component.html',
-  styleUrl: './todos.component.scss'
+  styleUrl: './todos.component.scss',
 })
-export class TodosComponent {
-
+export class TodosComponent implements OnInit {
+  todosService = inject(TodosService);
+  todoItems = signal<Array<Todo>>([]);
+  //01:05:50
+  ngOnInit(): void {
+    this.todosService
+      .getTodosFromApi()
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          throw err;
+        })
+      )
+      .subscribe((todos) => {
+        this.todoItems.set(todos);
+      });
+  }
 }
